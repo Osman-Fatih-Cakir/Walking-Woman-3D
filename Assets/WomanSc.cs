69caa7aed0db92445bfe4e805c7c3603c056779c
Osman Fatih Çakır <78075948+Osman-Fatih-Cakir@users.noreply.github.com>
2021-06-29 16:19:51 +0300

    Camera fix

--- Assets/WomanSc.cs
+++ Assets/WomanSc.cs
@@ -33,7 +33,7 @@ public class WomanSc : MonoBehaviour
         if (LHObject.GetComponent<LevelHandlerSc>().GameOn 
             && !LHObject.GetComponent<LevelHandlerSc>().GameOver) // If the game is on, the game is playable
         {
-            sinx = Mathf.Sin(Time.time) * 1.0f;
+            sinx = Mathf.Sin(Time.time) * 1.25f;
             transform.position = new Vector3(ipos.x + sinx, transform.position.y, transform.position.z); // Move woman around x axis
         }
     }
