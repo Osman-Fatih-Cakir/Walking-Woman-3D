b8b69cc2f09348592df61135962b6ffed265256c
Osman-Fatih-Cakir <cakirosmanfatih@gmail.com>
2021-08-04 16:59:51 +0300

    control fix

--- Assets/PlayerControls.cs
+++ Assets/PlayerControls.cs
@@ -7,10 +7,7 @@ public class PlayerControls : MonoBehaviour
     public GameObject CameraObj;
     public GameObject PlayerObject;
     public float thrust = 150f;
-    public float PlayerDefaultSpeed;
-    public float SlowDownRateUp = 0.95f;
-    public float SlowDownRatePressed = 0.85f;
-
+    private float PlayerDefaultSpeed;
     public Rigidbody PlayerRigidbody;
 
     private Vector2 lastMousePos;
@@ -72,6 +69,8 @@ public class PlayerControls : MonoBehaviour
             else
             {
                 Vector2 currentMousePos = Input.mousePosition;
+                currentMousePos.x = (currentMousePos.x * 10000) / Screen.width;
+                currentMousePos.y = (currentMousePos.y * 10000) / Screen.height;
                 if (lastMousePos == Vector2.zero)
                 {
                     lastMousePos = currentMousePos;
@@ -79,7 +78,7 @@ public class PlayerControls : MonoBehaviour
                 deltaPos = currentMousePos - lastMousePos;
                 lastMousePos = currentMousePos;
 
-                Vector3 force = new Vector3(deltaPos.x * thrust, 0, deltaPos.y * thrust * 1.35F);
+                Vector3 force = new Vector3(deltaPos.x * thrust, 0, deltaPos.y * thrust * 2.5F);
                 PlayerRigidbody.velocity = Vector3.zero;
                 PlayerRigidbody.AddForce(force, ForceMode.Acceleration);
 
